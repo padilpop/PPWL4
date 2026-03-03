66575608cbca392872f5fc730ceaf3f082e502ba
Fahdil Raihandi <fahdilraihandi2146@gmail.com>
2026-03-04 04:33:51 +0700

    PRAKTIKUM 1 - VALIDASI REQUEST BODY

---
+++ src/validation.ts
@@ -0,0 +1,23 @@
+import { Elysia, t } from "elysia";
+import { openapi } from "@elysiajs/openapi";
+
+const app = new Elysia()
+  .use(openapi())
+  .post("/request",
+    ({ body }) => {
+      return {
+        message: "Success",
+        data: body
+      }
+    },
+    {
+      body: t.Object({
+        name: t.String({ minLength: 3 }),
+        email: t.String({ format: "email" }),
+        age: t.Number({ minimum: 18 })
+      })
+    }
+  )
+  .listen(3000);
+
+console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
